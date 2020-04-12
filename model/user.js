71cb28dc5ba621b7c813f5cab39a233fab072ca0
최영훈 <c_yh0327@naver.com>
2020-04-12 19:18:23 +0900

    fet/ defined user Schema

---
+++ model/user.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+    name: {type: String, required: true},
+    password: {type: String, required: true},
+    profileImage: String,
+    groupCode: {type: String, default : null},
+    refreshToken: {type: String, default : null},
+    salt: String,
+    admin : [{
+        groupCode: {type: String, default:null},
+    }],
+    home:{
+        lat:{type:String, default: null} , // 위도
+        lon:{type:String, default:null},  // 경도
+        location:{type: String, default :null}
+    },
+},{minimize: false})
+
+module.exports = mongoose.model('user',userSchema)
