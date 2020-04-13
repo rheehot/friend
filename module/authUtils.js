69a6fb1a609bf3bd505d7093e40394a3ea812d6e
최영훈 <c_yh0327@naver.com>
2020-04-13 16:14:59 +0900

    feat/ auth token

--- module/authUtils.js
+++ module/authUtils.js
@@ -17,6 +17,27 @@ module.exports = {
                     message:"만료된 토큰입니다."
                 })
             }
+            if(result == -2){
+                return res.status(statusCode.UNAUTHORIZED).json({
+                    message:"유효하지 않은 토큰입니다."
+                })
+            }
+            if(result ==-3){
+                return res.status(statusCode.UNAUTHORIZED).json({
+                    message:"만료된 토큰입니다."
+                })
+            }
+            const userEmail = result.email 
+            console.log(userEmail)
+            if(!userEmail){
+                return res.status(statusCode.UNAUTHORIZED).json({
+                    message:"유효하지 않은 토큰입니다."
+                })
+            } else{
+                req.userEmail = userEmail
+                req.decoded = result
+                next()
+            }
         }
     }
 }
